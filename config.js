978182790189748a3026563166d3b66cb6337952
Sanatan <sanatannk@gmail.com>
2023-05-24 15:43:13 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: "db4free.net",
+    user: "rm28375",
+    password: "Zxcvbnm1@",
+    database: "nodejs_api",
+  },
+  listPerPage: 10,
+};
+module.exports = config;
